928c6ddaeb7f7f869649b2f7cc05161e28785813
aheller829 <allie.heller@briloon.org>
2025-04-15 16:53:30 -0400

    Updated 4/15/25

--- S123_ingest.R
+++ S123_ingest.R
@@ -5,13 +5,13 @@ library(readxl)
 
 ### Point to data folder
 
-dir <- "C:/Users/allie.heller/OneDrive - Biodiversity Research Institute/Desktop/Mafisa 2 Data/L1/"
+dir <- "C:/Users/allie.heller/OneDrive - Biodiversity Research Institute/Desktop/Mafisa 2 Data/"
 setwd(dir)
 
 
 # Mafisa_2 Soil and Biomass data
-mafisa2_soil_biomass <- excel_sheets("20250327_ZambiaSoilBiomass_L1.xlsx") # File path
-mafisa2_soil_biomass_list <- lapply(mafisa2_soil_biomass, function(x) read_excel("20250327_ZambiaSoilBiomass_L1.xlsx", sheet = x))  # Read excel file into list of excel sheets
+mafisa2_soil_biomass <- excel_sheets("L1/20250415_ZambiaSoilBiomass_L1.xlsx") # File path
+mafisa2_soil_biomass_list <- lapply(mafisa2_soil_biomass, function(x) read_excel("L1/20250415_ZambiaSoilBiomass_L1.xlsx", sheet = x))  # Read excel file into list of excel sheets
 names(mafisa2_soil_biomass_list ) <- mafisa2_soil_biomass # Pull sheet names
 list2env(mafisa2_soil_biomass_list, envir=.GlobalEnv) # Write each excel sheet to a separate dataframe 
 
@@ -116,8 +116,8 @@ Mafisa_2_data_join <- dplyr::mutate(Mafisa_2_data_join, FormVersion = "Mafisa_2 
 
 
 # If necessary, read in additional form version data and bind to dataframe
-og_mafisa2_soil_clean <- read.csv("ZambiaSoilC_L1.1.csv")
-og_dbh_clean <- read.csv("ZambiaSoilC_dbh_L1.1.csv")
+og_mafisa2_soil_clean <- read.csv("L1/ZambiaSoilC_L1.1.csv")
+og_dbh_clean <- read.csv("L1/ZambiaSoilC_dbh_L1.1.csv")
 
 
 
@@ -131,7 +131,7 @@ dbh_clean_join <- rbind(dbh_clean, og_dbh_clean)
 
 
 # Read in Kondwani's manually edited dbh excel data table
-dbh_edits <- read.csv("dbh_join_KondwaniEdits.csv")
+dbh_edits <- read.csv("L1/dbh_join_KondwaniEdits.csv")
 
 # Remove rows containing NA values
 dbh_edits <- na.omit(dbh_edits)
@@ -180,18 +180,14 @@ dbh_qc <- Mafisa_2_dbh_join  %>%
 
 
 # Write to csv
-write.csv(Mafisa_2_dbh_join, "Mafisa_2_dbh_20250414.csv", row.names = FALSE)
+write.csv(Mafisa_2_dbh_join, "L1/Mafisa_2_dbh_20250415.csv", row.names = FALSE)
 
-
-# QC check - tally how many observations per plot are in each original table
-# These numbers should remain consistent across tables
-table(dbh_clean_join$SoilPlot)
-table(dbh_edits$SoilPlot)
 
 
 
 
 # Create method-specific dataframes that are subsets of the whole dataset with potential errors
+# Fix errors as appropriate, and document in QC tracking
 # Bulk density QC
 bd_errors <- Mafisa_2_data_join %>%
   dplyr::select(ObjectID:BD_notes) %>%
@@ -200,6 +196,10 @@ bd_errors <- Mafisa_2_data_join %>%
                   BD_wet_mass_total < 5000 | # Lower expected BD value
                   WTD_mass < 250 | # Lower expected WTD value
                   WTD_mass > 600) # Upper expected WTD value
+
+# BD fixes
+Mafisa_2_data_join <- Mafisa_2_data_join %>%
+  dplyr::mutate(BD_depth = ifelse(is.na(BD_depth), 100, BD_depth)) # Total BD weight indicates that cores with missing depths were standard 100 cm) 
 
 
 # SOC/texture QC
@@ -212,22 +212,26 @@ soc_errors <- Mafisa_2_data_join %>%
                 SOC_combined != "yes" | # If SOC samples were not combined, why?
                 TXT_collected != "yes") # If texture sample was not collected, why?
 
+
 # Rootpit QC
 rootpit_qc <- Mafisa_2_data_join %>%
   dplyr::select(ObjectID:PlotNotes, RootPit_collected:RootPit_notes) %>%
   dplyr::filter(RootPit_collected == "yes" & is.na(RootPit_depth)) # If root pit was collected, depth should not be NA
                   
+
+
 # Percent cover and densiometer QC
 pct_qc <- Mafisa_2_data_join %>%
   dplyr::select(ObjectID:PlotNotes, Densi_collected:PCT_notes) %>%
   dplyr::mutate(LowerCanopyCover = DRH_pct + SRH_pct + SHRUB_pct + BARE_pct) %>% # Add a lower canopy column - this should not exceed 100
   dplyr::filter(Densi_collected == "yes" & is.na(DensiCanopyCover) | # If densiometer was used, canopy cover should not be NA
                 DensiCanopyCover > 100 | # Densiometer canopy cover should not exceed 100
-                PCT_collected == "yes" & if_any(c(TREES_pct, DRH_pct, SRH_pct, SHRUB_pct, BARE_pct), ~ is.na(.)) | # If percent cover was estimated, no funtional group should be NA
+                PCT_collected == "yes" & if_any(c(TREE_pct, DRH_pct, SRH_pct, SHRUB_pct, BARE_pct), ~ is.na(.)) | # If percent cover was estimated, no funtional group should be NA
                 TotalCover < 100 | # Total cover should not be below 100
                 TotalCover > 200 | # Total cover should not exceed 200
                 LowerCanopyCover > 100) # Lower canopy cover should not exceed 100, except perhaps in rare situations where shrub cover is high and shrubs have an unusual shape allowing for beneath shrub cover to be estimated
   
+
 # Biomass QC
 bio_qc <- Mafisa_2_data_join %>%
   dplyr::select(ObjectID:PlotNotes, Biomass_collected:Biomass_notes) %>%
@@ -237,3 +241,6 @@ bio_qc <- Mafisa_2_data_join %>%
                 HerbBio_weight > 2000 ) # Biomass weight should not exceed 2000
   
 
+
+# Save QC'd data 
+write.csv(Mafisa_2_data_join, "L1/Mafisa2_SoilBiomass_L1.1.csv", row.names = FALSE)
